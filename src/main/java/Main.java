dc7670cd7d9ee0149bc88752482cb1dd5e9cc554
Mirt12 <Mirt12@yandex.ru>
2023-06-08 19:26:45 +0200

    add Back2 to Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello group!");
-        System.out.println("I'll be back!");
+        System.out.println("I'll be back2!");
 
     }
 }
