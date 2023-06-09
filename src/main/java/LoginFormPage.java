668af25c9c838737c28e31e0e84074b0b8c9d355
olga-gasich <131393271+olga-gasich@users.noreply.github.com>
2023-06-09 21:25:04 +0300

    UI-040 - Login form page - add WebDriver field and Constructor with WebDriver parameter
    * add WebDriver driver field and constructor with WebDriver parameter
    
    * remove private access modifier from the fields
    
    ---------
    
    Co-authored-by: Sergei Tsarik <49017393+github4ta@users.noreply.github.com>

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -1,4 +1,13 @@
+import org.openqa.selenium.WebDriver;
+
 public class LoginFormPage {
+
+    WebDriver driver;
     String labelTitleEnterByClassName = "auth-form__title auth-form__title_big auth-form__title_condensed-default";
     String buttonLoginByClassName = "auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full";
+  
+    public LoginFormPage(WebDriver driver){
+
+        this.driver = driver;
+    }
 }
