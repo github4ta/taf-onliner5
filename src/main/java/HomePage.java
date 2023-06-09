3cfe3b64e94a8c320ca6dea7c1b79c478c01c77b
“Olga <olgazalmanova085@gmail.com>
2023-06-09 17:21:28 +0100

    add field WebDriver driver

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,5 +1,14 @@
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
     String footerClassName = "footer-style__copy";
     String buttonEnterByClassName = "auth-bar__item auth-bar__item--text";
     String buttonNewsByClassName = "b-main-navigation__text";
+    WebDriver driver;
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+    }
+
+
 }
