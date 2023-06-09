51c1fc76888ccbc5f48ddd8f712afcdfb334e4d9
Olga Gasich <olagasich@gmail.com>
2023-06-09 16:30:23 +0300

    remove private access modifier from the fields

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -2,8 +2,8 @@ import org.openqa.selenium.WebDriver;
 
 public class LoginFormPage {
 
-    private WebDriver driver;
-    private String labelTitleEnterByClassName = "auth-form__title auth-form__title_big auth-form__title_condensed-default";
+    WebDriver driver;
+    String labelTitleEnterByClassName = "auth-form__title auth-form__title_big auth-form__title_condensed-default";
 
     public LoginFormPage(WebDriver driver){
 
