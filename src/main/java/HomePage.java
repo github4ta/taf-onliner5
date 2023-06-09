0c820ce649cb2993c514cfcd1cfe46af3b8401f0
Alexey Lenkevich <lenkevich97@mail.ru>
2023-06-09 13:48:35 +0300

    add buttonNewsLByClassName

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,5 +1,5 @@
 public class HomePage {
     String footerClassName = "footer-style__copy";
     String buttonEnterByClassName = "auth-bar__item auth-bar__item--text";
-    String buttonNewsLByClassName ="b-main-navigation__text";
+    String buttonNewsLByClassName = "b-main-navigation__text";
 }
