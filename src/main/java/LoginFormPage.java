92d63c0ec869c497dc7664970eabfa0d5143044a
Olga Gasich <olagasich@gmail.com>
2023-06-09 14:44:31 +0300

    add WebDriver driver field and constructor with WebDriver parameter

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -1,3 +1,12 @@
+import org.openqa.selenium.WebDriver;
+
 public class LoginFormPage {
-    String labelTitleEnterByClassName = "auth-form__title auth-form__title_big auth-form__title_condensed-default";
+
+    private WebDriver driver;
+    private String labelTitleEnterByClassName = "auth-form__title auth-form__title_big auth-form__title_condensed-default";
+
+    public LoginFormPage(WebDriver driver){
+
+        this.driver = driver;
+    }
 }
