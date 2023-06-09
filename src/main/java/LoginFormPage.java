1d8bd58c6b3e56067f1a1e391aa65fd4b2bcae28
VladislavFilenkov <131393003+VladislavFilenkov@users.noreply.github.com>
2023-06-09 16:12:36 +0300

    UI-007 - Login form page - Button Login locator
    Co-authored-by: Vladislav Filenkov <n.thor@mail.ru>

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -1,3 +1,4 @@
 public class LoginFormPage {
     String labelTitleEnterByClassName = "auth-form__title auth-form__title_big auth-form__title_condensed-default";
+    String buttonLoginByClassName = "auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full";
 }
