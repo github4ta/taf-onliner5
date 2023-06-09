6299859e9fd3c72bb989a7845715ab59b6741afc
Vladislav Filenkov <n.thor@mail.ru>
2023-06-09 13:15:42 +0300

    add servicesItem locator

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,4 +1,5 @@
 public class HomePage {
     String footerClassName = "footer-style__copy";
     String buttonEnterByClassName = "auth-bar__item auth-bar__item--text";
+    String servicesItemByXpath = "//span[@class='b-main-navigation__text' and text() = 'Услуги']";
 }
