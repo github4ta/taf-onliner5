5570b0e4af744839fd1b06cf49f75a6f1a01f55f
Vladislav Filenkov <n.thor@mail.ru>
2023-06-09 16:21:25 +0300

    refactor code

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -1,4 +1,3 @@
 public class LoginFormPage {
     String labelTitleEnterByClassName = "auth-form__title auth-form__title_big auth-form__title_condensed-default";
-    String buttonLoginByClassName = "auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full";
 }
