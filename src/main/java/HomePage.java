0b5af17a7d3f1ffa2ea3fa39a586cd1c7220439a
Andrei Aliasiuk <aliasiuk13@gmail.com>
2023-06-09 20:31:06 -0700

    Add main nav Avtobaraholka menu item locator

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -3,4 +3,5 @@ public class HomePage {
     String buttonEnterByClassName = "auth-bar__item auth-bar__item--text";
     String servicesItemByXpath = "//span[@class='b-main-navigation__text' and text() = 'Услуги']";
     String buttonNewsByClassName = "b-main-navigation__text";
+    String mainNavAvtobaraholkaItemXpath ="//span[@class='b-main-navigation__text'and text()='Автобарахолка']";
 }
