b15a8f94a4ef05ccedd772e97c8970a6402040cd
Andrei Aliasiuk <aliasiuk13@gmail.com>
2023-06-09 17:11:52 -0700

    Add Password input field locator

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -4,6 +4,7 @@ public class LoginFormPage {
 
     WebDriver driver;
     String labelTitleEnterByClassName = "auth-form__title auth-form__title_big auth-form__title_condensed-default";
+    String inputPasswordFieldXpath = "//input[@type='password']";
     String buttonLoginByClassName = "auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full";
   
     public LoginFormPage(WebDriver driver){
