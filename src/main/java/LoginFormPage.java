620346e9b969415209642e1e670048ad4c17ee21
Andrei Aliasiuk <aliasiuk13@gmail.com>
2023-06-09 22:03:58 -0700

    UI-006 - Login form page - Password input locator

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -5,6 +5,7 @@ public class LoginFormPage {
     WebDriver driver;
   
     String labelTitleEnterByClassName = "auth-form__title auth-form__title_big auth-form__title_condensed-default";
+    String inputPasswordFieldXpath = "//input[@type='password']";
     String buttonLoginByClassName = "auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full";
     String inputNickNameByXPath ="//input[@placeholder='Ник или e-mail']";
   
