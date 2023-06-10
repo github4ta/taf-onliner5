b956349ddd88f9a00afd94865a1f3fba517be15a
“Olga <olgazalmanova085@gmail.com>
2023-06-10 17:43:48 +0100

    add clickServicesItem method

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,16 +1,22 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomePage {
     String footerClassName = "footer-style__copy";
     String buttonEnterByClassName = "auth-bar__item auth-bar__item--text";
     String buttonNewsByClassName = "b-main-navigation__text";
     String servicesItemByXpath = "//span[@class='b-main-navigation__text' and text() = 'Услуги']";
-    String mainNavAvtobaraholkaItemXpath ="//span[@class='b-main-navigation__text'and text()='Автобарахолка']";
-    String mainNavForumItemXpath ="//span[@class='b-main-navigation__text'and text()='Форум']";
-  
+    String mainNavAvtobaraholkaItemXpath = "//span[@class='b-main-navigation__text'and text()='Автобарахолка']";
+    String mainNavForumItemXpath = "//span[@class='b-main-navigation__text'and text()='Форум']";
+
     WebDriver driver;
 
-    public HomePage(WebDriver driver){
+    public HomePage(WebDriver driver) {
         this.driver = driver;
     }
-}
+        public void clickServicesItem() {
+            WebElement servicesItem = driver.findElement(By.xpath(servicesItemByXpath));
+            servicesItem.click();
+        }
+    }
