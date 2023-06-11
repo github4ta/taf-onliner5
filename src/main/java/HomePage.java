6dbb2deb03d97d116f18e1334f14a9428f6c4cba
Alexey Lenkevich <lenkevich97@mail.ru>
2023-06-11 10:16:03 +0300

    add method clickNewsButton

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -2,12 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class HomePage {
+public class HomePage{
     String footerClassName = "footer-style__copy";
     String buttonEnterByClassName = "auth-bar__item auth-bar__item--text";
     String buttonNewsByXpath = "//span[@class='b-main-navigation__text' and text()='Новости']";
     String servicesItemByXpath = "//span[@class='b-main-navigation__text' and text() = 'Услуги']";
-    String mainNavAvtobaraholkaItemXpath = "//span[@class='b-main-navigation__text' and text()='Автобарахолка']";
+    String mainNavAvtobaraholkaItemXpath ="//span[@class='b-main-navigation__text'and text()='Автобарахолка']";
+
 
     WebDriver driver;
 
