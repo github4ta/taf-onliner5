5707780f0542a4a450e071a756b4f6ea23ab1223
“Olga <olgazalmanova085@gmail.com>
2023-06-11 20:43:46 +0100

    add changes clickServicesItem method

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -7,14 +7,6 @@ public class HomePage {
     String buttonEnterByClassName = "auth-bar__item auth-bar__item--text";
     String buttonNewsByClassName = "b-main-navigation__text";
     String servicesItemByXpath = "//span[@class='b-main-navigation__text' and text() = 'Услуги']";
-    String mainNavAvtobaraholkaItemXpath = "//span[@class='b-main-navigation__text'and text()='Автобарахолка']";
-    String mainNavForumItemXpath = "//span[@class='b-main-navigation__text'and text()='Форум']";
-
-    WebDriver driver;
-
-    public HomePage(WebDriver driver) {
-        this.driver = driver;
-    }
         public void clickServicesItem() {
             WebElement servicesItem = driver.findElement(By.xpath(servicesItemByXpath));
             servicesItem.click();
