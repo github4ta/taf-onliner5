d0fdb66a405945bda1f43cab9cf96a4fb078ee01
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-06-11 23:30:07 +0300

    add methods before and after to Test class

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
+    WebDriver driver;
+    @BeforeEach
+    public void beforeTest(){
+        driver = new ChromeDriver();
+    }
+    @AfterEach
+    public void afterTest(){
+        driver.quit();
+    }
 }
