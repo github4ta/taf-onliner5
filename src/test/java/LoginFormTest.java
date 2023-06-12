b38d503185ee75bf968df8308c8cce84ab8f1aad
Avchinnikova Alina <avchinnikova1012@gmail.com>
2023-06-12 21:52:46 +0300

    add two Test

--- src/test/java/LoginFormTest.java
+++ src/test/java/LoginFormTest.java
@@ -1,2 +1,16 @@
-class LoginFormTest extends Test {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LoginFormTest extends Test{
+    @Test
+    public void inputEmailPlaceholderCheck() {
+        homePage.clickButtonEnter();
+        Assertions.assertEquals("Ник или e-mail",loginFormPage.inputNickName());
+    }
+    @Test
+    public void inputPasswordPlaceholderCheck() {
+        homePage.clickButtonEnter();
+        Assertions.assertEquals("Ник или e-mail",loginFormPage.inputPasswordCheck());
+    }
+
 }
