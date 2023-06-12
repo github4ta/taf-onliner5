e33a67b6c71319f35f3531fce0d2d6ba721eb023
Andrei Aliasiuk <aliasiuk13@gmail.com>
2023-06-12 10:16:46 -0700

    Refactor HomePage class (#48)

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -2,16 +2,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class HomePage{
+public class HomePage {
     String footerClassName = "footer-style__copy";
     String buttonEnterByClassName = "auth-bar__item auth-bar__item--text";
     String buttonNewsByXpath = "//span[@class='b-main-navigation__text' and text() = 'Новости']";
     String servicesItemByXpath = "//span[@class='b-main-navigation__text' and text() = 'Услуги']";
-    String mainNavAvtobaraholkaItemXpath ="//span[@class='b-main-navigation__text'and text()='Автобарахолка']";
-    String mainNavForumItemXpath ="//span[@class='b-main-navigation__text'and text()='Форум']";
+    String mainNavAvtobaraholkaItemXpath = "//span[@class='b-main-navigation__text'and text()='Автобарахолка']";
+    String mainNavForumItemXpath = "//span[@class='b-main-navigation__text'and text()='Форум']";
     String mainNavFleaItemByXpath = "//span[@class='b-main-navigation__text'][text()='Барахолка']";
     String buttonCatalogItemXpath = "//span[@class='b-main-navigation__text' and text() = 'Каталог']";
-    String buttonHousesAndApartmentsXPath= "//*[@id='container']/div/div[2]/header/div[1]/div/nav/ul[1]/li[4]/a/span";
+    String buttonHousesAndApartmentsXPath = "//*[@id='container']/div/div[2]/header/div[1]/div/nav/ul[1]/li[4]/a/span";
 
     WebDriver driver;
 
@@ -23,28 +23,30 @@ public class HomePage{
         WebElement buttonNews = driver.findElement(By.className(buttonNewsByXpath));
         buttonNews.click();
         return this;
-
-        public void clickButtonEnter(){
-            WebElement buttonEnter = driver.findElement(By.className(buttonEnterByClassName));
-            buttonEnter.click();
-        }
-        public void clickmainNavAvtobaraholkaItem(){
-            Webelement mainNavAvtobaraholkaItem = driver.findElement(By.Xpath(mainNavAvtobaraholkaItemXpath));
-            mainNavAvtobaraholkaItem.click();
-        }
-
-        public void clickServicesItem() {
-            WebElement servicesItem = driver.findElement(By.xpath(servicesItemByXpath));
-            servicesItem.click();
-        }
     }
 
-      public HomePage clickMenuItemFlea(){
+    public void clickButtonEnter() {
+        WebElement buttonEnter = driver.findElement(By.className(buttonEnterByClassName));
+        buttonEnter.click();
+    }
+
+    public void clickmainNavAvtobaraholkaItem() {
+        WebElement mainNavAvtobaraholkaItem = driver.findElement(By.xpath(mainNavAvtobaraholkaItemXpath));
+        mainNavAvtobaraholkaItem.click();
+    }
+
+    public void clickServicesItem() {
+        WebElement servicesItem = driver.findElement(By.xpath(servicesItemByXpath));
+        servicesItem.click();
+    }
+
+    public HomePage clickMenuItemFlea() {
         WebElement menuItemFlea = driver.findElement(By.xpath(mainNavFleaItemByXpath));
         menuItemFlea.click();
         return this;
+    }
 
-    public void clickbuttonCatalogItem(){
+    public void clickbuttonCatalogItem() {
         WebElement buttonCatalogItem = driver.findElement(By.xpath(buttonCatalogItemXpath));
         buttonCatalogItem.click();
     }
