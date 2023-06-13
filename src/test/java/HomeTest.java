7752cb524de94c1cd8871d25b57fb52d364f9856
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-06-13 16:05:10 +0300

    add method openSiteAndCheckFooterText

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+
 class HomeTest extends Test {
+
+    HomePage homePage;
+    @org.junit.jupiter.api.Test
+    public void openSiteAndCheckFooterText(){
+        homePage = new HomePage(driver);
+        homePage.goTo();
+        String actualResult = homePage.getFooterText();
+        Assertions.assertEquals("© 2001—2023 Onlíner", actualResult);
+    }
 }
