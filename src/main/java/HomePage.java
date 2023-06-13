683c8ef5592615db8d3d5b044fc89b139a1aca37
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-06-13 15:40:37 +0300

    delete string url from branch master

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -12,7 +12,6 @@ public class HomePage {
     String mainNavFleaItemByXpath = "//span[@class='b-main-navigation__text'][text()='Барахолка']";
     String buttonCatalogItemXpath = "//span[@class='b-main-navigation__text' and text() = 'Каталог']";
     String buttonHousesAndApartmentsXPath = "//*[@id='container']/div/div[2]/header/div[1]/div/nav/ul[1]/li[4]/a/span";
-    String url = "https://www.onliner.by/";
     WebDriver driver;
 
     public HomePage(WebDriver driver) {
