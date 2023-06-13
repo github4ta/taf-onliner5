f15094adfc4566cdb5c16cd3bc7fed252656ca79
Sergei Tsarik <serge.tsarik@gmail.com>
2023-06-13 21:01:26 +0300

    reformat code

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -3,13 +3,4 @@ import org.junit.jupiter.api.Test;
 
 class HomeTest extends BaseTest {
 
-    HomePage homePage;
-
-    @Test
-    public void openSiteAndCheckFooterText() {
-        homePage = new HomePage(driver);
-        homePage.goTo();
-        String actualResult = homePage.getFooterText();
-        Assertions.assertEquals("© 2001—2023 Onlíner", actualResult);
-    }
 }
--- src/test/java/LoginFormTest.java
+++ src/test/java/LoginFormTest.java
@@ -3,19 +3,4 @@ import org.junit.jupiter.api.Test;
 
 class LoginFormTest extends BaseTest {
 
-    @Test
-    public void inputEmailPlaceholderCheck() {
-        HomePage homePage = new HomePage(driver);
-        homePage.clickButtonEnter();
-        LoginFormPage loginFormPage = new LoginFormPage(driver);
-        Assertions.assertEquals("Ник или e-mail", loginFormPage.inputNickName());
-    }
-
-    @Test
-    public void inputPasswordPlaceholderCheck() {
-        HomePage homePage = new HomePage(driver);
-        homePage.clickButtonEnter();
-        LoginFormPage loginFormPage = new LoginFormPage(driver);
-        Assertions.assertEquals("Ник или e-mail", loginFormPage.inputPasswordCheck());
-    }
 }
