ffe3cbd7f546c9f2f9c4e7b3d62427e07aebe418
Mirt12 <Mirt12@yandex.ru>
2023-06-13 20:45:31 +0200

    add UI-604

--- src/test/java/LoginFormTest.java
+++ src/test/java/LoginFormTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 class LoginFormTest extends BaseTest {
 
@@ -14,4 +17,16 @@ class LoginFormTest extends BaseTest {
 
         Assertions.assertEquals("Вход", actual);
     }
+
+    @Test
+    public void testIsCorrectNameButtonSignIn(){
+        WebDriver driver = new ChromeDriver();
+        HomePage homePage = new HomePage(driver);
+        LoginFormPage page = new LoginFormPage(driver);
+
+        homePage.goTo();
+        homePage.clickButtonEnter();
+        Assertions.assertEquals("Войти", page.getTextOfBtnLogin());
+    }
+
 }
