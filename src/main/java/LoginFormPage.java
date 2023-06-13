e711e5305c147c66d93b8a8792733510fa6cd515
“Olga <olgazalmanova085@gmail.com>
2023-06-13 10:58:32 +0100

    add test Login Header text

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -1,22 +1,29 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginFormPage {
 
     WebDriver driver;
-  
     String labelTitleEnterByClassName = "auth-form__title auth-form__title_big auth-form__title_condensed-default";
     String inputPasswordFieldXpath = "//input[@type='password']";
     String buttonLoginByClassName = "auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full";
-    String inputNickNameByXPath ="//input[@placeholder='Ник или e-mail']";
+    String inputNickNameByXPath = "//input[@placeholder='Ник или e-mail']";
     String errorMessageEmailXpath = "//div[contains(@class,'auth-form__description_error') and contains(text(), 'Укажите ник или e-mail')]";
     String errorMessagePasswordXpath = "//div[contains(@class,'auth-form__description_error') and contains(text(), 'Укажите пароль')]";
+    String resultTextLogin;
 
+    public LoginFormPage(WebDriver driver) {
 
-    public LoginFormPage(WebDriver driver){
         this.driver = driver;
     }
-    public void clickButtonLogin(){
+
+    public void clickButtonLogin() {
+
         driver.findElement(By.className(buttonLoginByClassName)).click();
+    }
+    public String getResultText() {
+        WebElement resultField = driver.findElement(By.xpath(labelTitleEnterByClassName));
+        return resultField.getText();
     }
 }
--- src/test/java/LoginFormTest.java
+++ src/test/java/LoginFormTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class LoginFormTest extends Test {
+
+    @Test
+    public void testLoginHeader(){
+        driver.get("https://www.onliner.by/");
+        homePage.clickButtonEnter();
+        String actualResult = loginFormPage.getResultTextLogin();
+        Assertions.assertEquals(actualResult, "Вход");
+    }
+    @Test
+    public void inputEmailPlaceholderCheck() {
+        homePage.clickButtonEnter();
+        Assertions.assertEquals("Ник или e-mail",loginFormPage.inputNickName());
+    }
 }
