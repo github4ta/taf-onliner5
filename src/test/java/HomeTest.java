5e2444a4a353362cbe3fcbb9361f75b8b0330033
Katerina Shidlovskaya <katerinashidlovskaya2711@gmail.com>
2023-06-14 11:06:04 +0300

    add checkHomepageOpenedTest +BeforeEach and AfterEach

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,6 +1,28 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 class HomeTest extends BaseTest {
+    ChromeDriver driver;
+    HomePage homePage;
 
+    @BeforeEach
+    public void warmUp() {
+        driver = new ChromeDriver();
+        homePage = new HomePage(driver);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testCheckHomepageOpened() {
+        homePage.goTo();
+        String expectedMessage = homePage.getFooterText();
+        Assertions.assertEquals(expectedMessage,"© 2001—2023 Onlíner");
+    }
+    @AfterEach
+    public void tearsDown() {
+        driver.quit();
+    }
 }
