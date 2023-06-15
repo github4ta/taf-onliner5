8155a3f9dc7549ca92a263499bd48587efda5c05
KaterinaShidlovskaya <131393342+KaterinaShidlovskaya@users.noreply.github.com>
2023-06-15 22:08:14 +0300

    UI-042 - Login form page - get text title method (#82)
    * add formTitleLocator
    
    * add getTextTitleForm method to LoginFormPage
    
    ---------
    
    Co-authored-by: Sergei Tsarik <49017393+github4ta@users.noreply.github.com>

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -1,4 +1,6 @@
 public class LoginFormPage {
+  
+    WebDriver webdriver;
 
     String titleFormLocator = "//*[@id='auth-container']/div/div[2]/div/div[1]";
     String passwordInputLocator = "//input[@type='password']";
@@ -7,8 +9,17 @@ public class LoginFormPage {
     String errorMassageEmailLocator = "/div[contains(text(),'Укажите ник или e-mail')]";
     String errorMassageEmailLocator = "//div[@class='auth-form__description auth-form__description_error auth-form__description_base auth-form__description_extended-other' and text()='Укажите ник или e-mail']";
 
+    public LoginFormPage(WebDriver webdriver) {
+        this.webdriver = webdriver;
+    }
+
     public void clickButtonEnter(){
         webDriver.findElement(By.xpath(enterButtonLocator)).click();
+    }
+  
+    public String getTextTitleForm(){
+        String textTitleForm = (webdriver.findElement(By.xpath(titleFormLocator))).getText;
+        return textTitleForm;
     }
 }
 
