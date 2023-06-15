faa7e66b1eb40cd8294b5b2fa4dcc793e086a146
“Olga <olgazalmanova085@gmail.com>
2023-06-15 18:29:08 +0100

    add item Auto

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -4,6 +4,8 @@ public class HomePage {
     private String getTextLocatorXpath="/html/body/div[1]/footer/div/div/div/div[2]/div[2] ";
     private String mainMenuNewsItemLocator = "//span[@class='b-main-navigation__text' and text()='Новости']";
 
+    private String MainMenuAutoItemLocator = "//*[@id='container']/div/div/header/div[2]/div/nav/ul[1]/li[3]/a/span";
+
     public HomePage(ChromeDriver driver) {
         this.driver = driver;
     }
