2a9c70a08cae94aeb2beb76e8bb71d500d6e6872
Avchinnikova Alina <avchinnikova1012@gmail.com>
2023-06-15 21:38:20 +0300

    add locator

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -2,6 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HomePage {
 
     WebDriver driver;
--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -4,4 +4,5 @@ public class LoginFormPage {
     String passwordInputLocator = "//input[@type='password']";
     String nicknameInputLocator = "//input[@placeholder='Ник или e-mail']";
     String enterButtonLocator = "//button[@type='submit' and @class='auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full']";
+    String errorMassageEmailLocator = "//div[@class='auth-form__description auth-form__description_error auth-form__description_base auth-form__description_extended-other' and text()='Укажите ник или e-mail']";
 }
