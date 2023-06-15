1cbf46d56edb8bebcb7c4c5840a30dd98f62f8ce
Mirt12 <Mirt12@yandex.ru>
2023-06-15 19:20:41 +0200

    add locator to button Enter

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -1,3 +1,3 @@
 public class LoginFormPage {
+    String btnEnterLocatorByXPath = "//button[@type='submit' and @class='auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full']";
 }
-
