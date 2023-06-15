a85749dccb15fa04be4037c7a7d01bafbc34151a
Andrei Aliasiuk <aliasiuk13@gmail.com>
2023-06-15 10:56:46 -0700

    UI-003 - Add Login button locator
    Co-authored-by: Sergei Tsarik <49017393+github4ta@users.noreply.github.com>

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,10 +1,11 @@
 public class HomePage {
 
     private ChromeDriver driver;
+  
     private String getTextLocatorXpath="/html/body/div[1]/footer/div/div/div/div[2]/div[2] ";
     private String mainMenuNewsItemLocator = "//span[@class='b-main-navigation__text' and text()='Новости']";
+    private String loginButtonLocator = "auth-bar__item auth-bar__item--text";
     private String mainMenuHousesAndApartmentsByXpath = "//span[@ class='b-main-navigation__text' and text() = 'Дома и квартиры']";
-
     private String MainMenuAutoItemLocator = "//*[@id='container']/div/div/header/div[2]/div/nav/ul[1]/li[3]/a/span";
 
     public HomePage(ChromeDriver driver) {
