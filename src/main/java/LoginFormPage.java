ad51bf287062e1f32020666e6d62996c9fdda076
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-06-15 21:43:36 +0300

    add clickButtonEnter (#77)

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -1,7 +1,13 @@
 public class LoginFormPage {
+    Webdriver webdriver;
 
     String titleFormLocator = "//*[@id='auth-container']/div/div[2]/div/div[1]";
     String passwordInputLocator = "//input[@type='password']";
     String nicknameInputLocator = "//input[@placeholder='Ник или e-mail']";
     String enterButtonLocator = "//button[@type='submit' and @class='auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full']";
+
+    public void clickButtonEnter(){
+        webDriver.findElement(By.xpath(enterButtonLocator)).click();
+    }
 }
+
