e4982a01362c6653ef3617a41c487c4f3c941299
Andrei Aliasiuk <aliasiuk13@gmail.com>
2023-06-15 10:31:12 -0700

    Added login button locator

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -3,6 +3,7 @@ public class HomePage {
     private ChromeDriver driver;
     private String getTextLocatorXpath="/html/body/div[1]/footer/div/div/div/div[2]/div[2] ";
     private String mainMenuNewsItemLocator = "//span[@class='b-main-navigation__text' and text()='Новости']";
+    private String loginButtonLocator = "auth-bar__item auth-bar__item--text";
 
     public HomePage(ChromeDriver driver) {
         this.driver = driver;
