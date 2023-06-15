fc9a2c781a35e1442eba673548d633281d2d6d27
Katerina Shidlovskaya <katerinashidlovskaya2711@gmail.com>
2023-06-15 20:24:04 +0300

    add formTitleLocator

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -1,4 +1,5 @@
 public class LoginFormPage {
 
     String inputPasswordFieldXpath = "//input[@type='password']";
+    String formTitleLocatorByXpath = "//*[@id='auth-container']/div/div[2]/div/div[1]";
 }
