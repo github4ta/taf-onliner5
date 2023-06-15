1fdd40246d2d3dfa2a0e7c64cd3295220ccdbe10
Avchinnikova Alina <avchinnikova1012@gmail.com>
2023-06-15 21:50:35 +0300

    refactor locator

--- src/main/java/LoginFormPage.java
+++ src/main/java/LoginFormPage.java
@@ -1,11 +1,10 @@
 public class LoginFormPage {
-  
-    Webdriver webdriver;
 
     String titleFormLocator = "//*[@id='auth-container']/div/div[2]/div/div[1]";
     String passwordInputLocator = "//input[@type='password']";
     String nicknameInputLocator = "//input[@placeholder='Ник или e-mail']";
     String enterButtonLocator = "//button[@type='submit' and @class='auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full']";
+    String errorMassageEmailLocator = "/div[contains(text(),'Укажите ник или e-mail')]";
     String errorMassageEmailLocator = "//div[@class='auth-form__description auth-form__description_error auth-form__description_base auth-form__description_extended-other' and text()='Укажите ник или e-mail']";
 
     public void clickButtonEnter(){
