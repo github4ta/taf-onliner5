e47ce89e9bf0db07ca7438cff9d72242a5542d86
Mikita Shykin <mikita.shikin@gmail.com>
2023-06-16 17:25:50 +0200

    add method clickItemCatalog

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -2,9 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class HomePage {
     WebDriver driver;
