c7194bfd2cf64218d752f26352713fa9d0655566
Mirt12 <Mirt12@yandex.ru>
2023-06-17 23:02:05 +0200

    add method clickMenuItemBaraholka() to HomeTest class

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -15,7 +15,7 @@ public class HomePage {
     private String mainMenuCatalogItemLocator = "//span[@class='b-main-navigation__text' and text()='Каталог']";
     private String mainMenuAutoItemLocator = "//*[@id='container']/div/div/header/div[2]/div/nav/ul[1]/li[3]/a/span";
     private String mainMenuForumItemLocator = "//span[@class='b-main-navigation__text'and text()='Форум']";
-    private String mainMenuBaraholkaItemLocator = "//*[@id='container']/div/div/header/div[2]/div/nav/ul[1]/li[6]/a/span";
+    private String mainMenuBaraholkaItemLocator = "//span[@class='b-main-navigation__text' and contains(text(),'Барахолка')]";
     private String mainMenuUslugiLocator = "//span[@class='b-main-navigation__text' and contains(text(), 'Услуги')]";
     private String loginButtonLocator = "auth-bar__item auth-bar__item--text";
 
@@ -31,5 +31,8 @@ public class HomePage {
     }
     public void clickMenuItemUslugi(){
         driver.findElement(By.xpath(mainMenuUslugiLocator)).click();
+    }
+
+    public void clickMenuItemBaraholka(){driver.findElement(By.xpath(mainMenuBaraholkaItemLocator)).click();
     }
 }
